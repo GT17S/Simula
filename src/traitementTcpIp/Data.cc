27cfceff6101e5ddbd0c28d91956ee8ed0eafe1e
Akli Hamitouche <akli.m.hamitouche@gmail.com>
2020-05-04 06:30:40 +0200

    Premiere versions des encapsulations desencapsulations (sans calcul de checksum)

--- src/traitementTcpIp/Data.cc
+++ src/traitementTcpIp/Data.cc
@@ -1,16 +1,14 @@
-#include "../../include/traitementTcpIp/Data.hh"
+#include "Data.hh"
 
 Data::Data(std::string message){
-	//std::cout << "Message: " <<  message << std::endl;
-	std::reverse(std::begin(message), std::end(message));
-	//std::cout << "Message: " <<  message << std::endl;
-	seq = new boost::dynamic_bitset<unsigned char>(std::begin(message),std::end(message));
+//	std::reverse(std::begin(message), std::end(message));
+	std::string s = this->strtobinary ( message);
+	seq = new boost::dynamic_bitset<>( s);
 	assert(seq);
-	
 	type = DATA_TOTAL;
 }
 
-Data::Data(boost::dynamic_bitset<unsigned char>* seq, data_t type){
+Data::Data(boost::dynamic_bitset<>* seq, data_t type){
 	setType(type);
 	setSeq(seq);
 }
@@ -24,7 +22,7 @@ void Data::setType(data_t _type){
 	this->type = _type;
 }
 
-void Data::setSeq(boost::dynamic_bitset<unsigned char>* _seq){
+void Data::setSeq(boost::dynamic_bitset<>* _seq){
 	assert(_seq);
 	this->seq = _seq;
 }
@@ -37,17 +35,32 @@ int Data::operator[](int index){
 }
 
 std::ostream& operator<<(std::ostream &os, Data& d){	
+	boost::dynamic_bitset<> tmp = *d.getSeq();
+	os << "Contenu : \n" ;
+	for (unsigned int i = 0; i < d.getSeq()->size(); i++)	{
+		if ( (i) % 32 == 0) os <<std::endl;
+		os << tmp[i];
+	}
 	
-	os << "Contenu" << *d.getSeq() << std::endl;
-	os << "Type: " << d.getType() << std::endl;
+	os << std::endl;
+	std::string s;
+	if (d.getType() == DATA_TOTAL) s = "Donnée";
+	else if (d.getType() == DATA_SEGMENT) s = "Segment";
+	else if (d.getType() == DATA_PAQUET) s = "Paquet";
+	else if (d.getType() == DATA_TRAME) s = "Trame";
+	os << "Type : " << s << std::endl;
 	return os;
 }
 
 std::string Data::strtobinary(std::string msg){
-	std::string binaryString = "";
-    for (char& _char : msg) {
-        binaryString += std::bitset<8>(_char).to_string();
-    }
-    //std::cout << "Binary: " <<  binaryString << std::endl;
+	std::string binaryString = "";	
+	const char * str = msg.c_str();
+	for (int i = (int) msg.size()-1; i > -1 ; i--)	{
+		char c = str[i];
+		for (int j = 0; j < 8; j++)	{
+			bool bit = c & (1<<j);
+			binaryString+= bit ? "1" : "0";
+		}
+	}
     return binaryString;
 }

18d1167bc95ed8eb28fb8418e255b322151ca01b
Mahdi LARBI <mahdi.larbi@ens.uvsq.fr>
2020-05-08 23:43:17 +0200

    Envoyer message #1

--- src/Simula.cc
+++ src/Simula.cc
@@ -16,8 +16,6 @@ void envoyer(Noeud * n1, Noeud * n2, vector<Cable*> path){
     extremite * ext1, * ext2;
     Cable * cable;
 
-    // 0 6 8 9 10 7 5
-
    // cable = path[--size_p];
    // ext1 = cable->getExt(n1); // 0
     Noeud * n = n1;
@@ -31,6 +29,7 @@ void envoyer(Noeud * n1, Noeud * n2, vector<Cable*> path){
 
         n = ext2->noeud;
     }
+    std::cout << n->getIdNoeud() << std::endl;
 
 }
 
@@ -46,41 +45,7 @@ int main( int argc, char ** argv)	{
     vector<Cable*> path;
     graphe->genererChemin(n1, n1, n2, path);
 
-    // cable last (src, suivant)
-    // cable last-1 (suivant , suivant+1)
-    // cable last-i (suivant +1, suivant+i)
-    // cable 0 (suivant+size-1, dest)
-
-    // ext 1 *;
-    // ext 2 *;
-    // cable 1
-    // ext1 = cable1->getExt(src)
-    // ext2 = cable1->Inverse(ext1->noeud);
-    // envoyer (ext1, ext2)
-    // cable 2
-    // ext1 = cable2 ->getExt(ex2->noeud);
-    // ext2 = cable2 ->Inverse(ext1->noeud);
-    // envoyer(ext1, ext2)
-
-
-
-    std::cout <<path.size()<<std::endl;
-    for (int i = path.size()-1; i > -1; i--) {
-        std::cout<<"Cable "<<i<<"("<<path[i]->getExt1()->noeud->getIdNoeud()<<
-                   " ,"<<path[i]->getExt2()->noeud->getIdNoeud()<<" )"<<std::endl;
-    }
-    std::cout<<std::endl;
-
     envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], path);
-    /*
-    int i = path.size() -1;
-    extremite * x  = path[i]->getExt1();
-    do{
-        std::cout<< x->noeud->getIdNoeud()<<" envoie à "<<std::endl;
-        x = path[i]->getExt(x->noeud);
-        i--;
 
-    }while(i > -1);
-*/
 
 }

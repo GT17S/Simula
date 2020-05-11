7e2b5a77faf8327daff5dab96e8e725486f1df97
LARBI Mahdi <larbi.mahdi@outlook.fr>
2020-05-12 00:10:51 +0200

    Update Simula.cc

--- src/Simula.cc
+++ src/Simula.cc
@@ -1,43 +1,16 @@
-#include <iostream>
-#include <vector>
-#include "GFichier.hh"
-#include "Graphe.hh"
-#include "DataOutils.hh"
+#include <QApplication>
+#include <QtWidgets>
 
-using std::vector;
+#include "simulaGui.hh"
+
 
 
 int main( int argc, char ** argv)	{
+    QApplication app(argc,argv);
+    simulaGui test;
+    test.show();
 
-    string message = argv[1];
-    int n1 = atoi(argv[2]);
-    int n2 = atoi(argv[3]);
 
-    Graphe * graphe = Graphe::get();
-    lireXml("test.xml", graphe);
+    return app.exec();
 
-    vector<Cable*> path;
-    //graphe->genererChemin(n1, n1, n2, path, true);
-
-     //string data = "Salam";
-    Data * data = new Data(message);
-    Data * data2 = new Data ("azul");
-    Data * data3 = new Data("bye");
-
-    Station * st = dynamic_cast<Station*>(graphe->getSommets()[n1]);
-    Station * st5 = dynamic_cast<Station*>(graphe->getSommets()[5]);
-    st5->setIsPasserelle(true);
-     envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, false, false, st->getNextNumSeq(), 0, 100, data);
-     //envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, true, false, st->getNextNumSeq(), 0, 101, data2);
-     //envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, true, false, st->getNextNumSeq(), 0, 102, data3);
-   // Graphe::genererChemin(0, n1 ,n2, path, false);
-
-     //std::cout << st->getControleur()->getMapFileEnvoyer().size()<<std::endl;
-     st->getControleur()->verifieNbrSegment(st);
-
-     delete graphe;
-     delete data;
-     delete data2;
-     delete data3;
 }
-/////

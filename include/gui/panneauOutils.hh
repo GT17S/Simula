915c8d60dca0bfda7dd8cc52163dfc4376dd0f18
GhilesBM <rilesbabou1@gmail.com>
2020-05-12 17:07:41 +0200

    ajout de la classe panneauOutils, j'ai laisser quelque ligne en commentaire,c'est à completer une fois la classe espace de travail fini

--- include/gui/panneauOutils.hh
+++ include/gui/panneauOutils.hh
@@ -2,27 +2,33 @@
 #define __H_PANNEAUOUTILS__ 
 
 #include <QToolBar>
-
+#include <QWidget>
+#include <QAction>
+#include <iostream>
+#include "EspaceTravail.hh"
 class PanneauOutils : public QToolBar{
 	Q_OBJECT
 public:
+
     PanneauOutils();
 	virtual ~PanneauOutils(){};
 
 public slots:
-	void nouveauFichier(){};
-	void ouvrirFichier(){};
-	void sauvegarderFichier(){};
-	void exporterFichier(){};
-	void demarrerSimulation(){};
-	void pauseSimulation(){};
-	void arreterSimulation(){};
-	void resetSimulation(){};
-	void changeMode(){};
-	void envoieD(){};
+    void nouveauFichier();
+    void ouvrirFichier();
+    void sauvegarderFichier();
+    void exporterFichier();
+    void exporterImage(){std::cout<< "Exporter"<<std::endl;};
+    void demarrerSimulation(){std::cout<< "demarrer"<<std::endl;};
+    void arreterSimulation(){std::cout<< "arreter"<<std::endl;};
+    void resetSimulation(){std::cout<< "reset"<<std::endl;};
+    void changeMode(){std::cout<< "changeMode"<<std::endl;};
+    void envoieD(){std::cout<< "envoieD"<<std::endl;};
 
+private :
+    QString curFile;
 
-
+    bool maybeSave();
 };
 
 #endif
--- src/gui/panneauOutils.cc
+++ src/gui/panneauOutils.cc
@@ -1,11 +1,135 @@
 #include "panneauOutils.hh"
-
+#include <QFileDialog>
+#include <QMessageBox>
+#include <QStatusBar>
+#include <QPixmap>
+#include <QPlainTextEdit>
+#include "GFichier.hh"
 PanneauOutils::PanneauOutils(){
 
-    this->setStyleSheet("background-color: green");
+   // this->setStyleSheet("background-color: green");
     this->setMinimumHeight(60);
     this->setMaximumHeight(70);
+    this->setOrientation(Qt::Horizontal);
+
+    QPixmap New("../../ressources/ajouterFichier.png");
+ // New.size().setHeight(5);
+ // New.size().setWidth(4);
+    QPixmap open("../../ressources/ouvrirFichier.png");
+    QPixmap save("../../ressources/sauvegarder.png");
+    QPixmap exporte("../../ressources/exporter.jpeg");
+    QPixmap png("../../ressources/img.png");
+    QPixmap start("../../ressources/demarrer.jpeg");
+    QPixmap stop("../../ressources/stop.jpeg");
+    QPixmap relance("../../ressources/relancer.png");
+    QPixmap change("../../ressources/changeMode.jpeg");
+    QPixmap send("../../ressources/send.jpeg");
+    //New.scaled(QSize(60,60),Qt::IgnoreAspectRatio);
+    //Bouttons
+    QAction *nouveau = this->addAction(QIcon(New),"Nouveau fichier");
+    QAction *ouvrir = this->addAction(QIcon(open),"Ouvrir fichier");
+    QAction *sauvegarder =this->addAction(QIcon(save),"Sauvegarder fichier");
+    QAction *exporter = this->addAction(QIcon(exporte),"Exporter fichier");
+    QAction *exporterPng=this->addAction(QIcon(png),"Exporter en Image");
+    this->addSeparator();
+    QAction *demarrer = this->addAction(QIcon(start),"demarrer Simulation");
+    QAction *arreter = this->addAction(QIcon(stop),"Arreter Simulation");
+    QAction *relancer = this->addAction(QIcon(relance),"Relancer Simulation");
+    QAction *changerMode = this->addAction(QIcon(change),"Changer mode");
+    QAction *envoyer = this->addAction(QIcon(send),"Envoyer Message");
+
+    //Racourcis
+    nouveau->setShortcut(QKeySequence("Ctrl+N"));
+    ouvrir->setShortcut(QKeySequence("Ctrl+O"));
+    sauvegarder->setShortcut(QKeySequence("Ctrl+S"));
+    exporter->setShortcut(QKeySequence("Ctrl+E"));
+    demarrer->setShortcut(QKeySequence("Ctrl+D"));
+    arreter->setShortcut(QKeySequence("Ctrl+Q"));
+
+
+    //connextion SIGNAL-SLOT
+    connect(nouveau,SIGNAL(triggered()),this,SLOT(nouveauFichier()));
+    connect(ouvrir,SIGNAL(triggered()),this,SLOT(ouvrirFichier()));
+    connect(sauvegarder,SIGNAL(triggered()),this,SLOT(sauvegarderFichier()));
+    connect(exporter,SIGNAL(triggered()),this,SLOT(exporterFichier()));
+    connect(exporterPng,SIGNAL(triggered()),this,SLOT(exporterImage()));
+    connect(demarrer,SIGNAL(triggered()),this,SLOT(demarrerSimulation()));
+    connect(arreter,SIGNAL(triggered()),this,SLOT(arreterSimulation()));
+    connect(relancer,SIGNAL(triggered()),this,SLOT(resetSimulation()));
+    connect(changerMode,SIGNAL(triggered()),this,SLOT(changeMode()));
+    connect(envoyer,SIGNAL(triggered()),this,SLOT(envoieD()));
+
+
 }
 
-
-
+void PanneauOutils::ouvrirFichier(){
+    QString fileName=QFileDialog::getOpenFileName(this,
+                                               tr("Open Address Book"), "",
+                                               tr("Address Book (*.xml)"));
+                            /* QTextEdit *feuille=new QTextEdit;
+                             feuille->setText(texte);*/
+   curFile=fileName;
+    //Graphe *g=new(); il faut rendre le constructeur public
+    //lireXml(fileName,g);//appliquer le graphe sur la scene
+   std::cout<<curFile.toStdString()<<std::endl;
+   QMessageBox::information(NULL, "Information!", "Fichier importer avec succé");
+}
+void PanneauOutils::sauvegarderFichier()
+{
+    if(curFile.isEmpty())
+    {
+         QString fileName = QFileDialog::getSaveFileName(this,
+            tr("Save Address Book"), "",
+             tr("Address Book (*.xml)"));
+         curFile=fileName;
+        if (fileName.isEmpty())
+              return;
+         else {
+             QFile file(fileName);
+                 if (!file.open(QIODevice::WriteOnly)) {
+                      QMessageBox::information(this, tr("Unable to open file"),
+                        file.errorString());
+                        return;
+                    }
+         }
+    }
+    //ecrireXml(curFile,EspaceTravail::g); //je dois acceder a la classe
+    QMessageBox::information(NULL, "Information!", "Fichier sauvegarder !");
+}
+void PanneauOutils::exporterFichier()
+{
+    QString fileName = QFileDialog::getSaveFileName(this,
+        tr("Save Address Book"), "",
+        tr("Address Book (*.dot)")); //confirmer comment s'ecrit l extention
+    curFile=fileName;
+    if (fileName.isEmpty())
+       return;
+    else {
+       QFile file(fileName);
+         if (!file.open(QIODevice::WriteOnly)) {
+             QMessageBox::information(this, tr("Unable to open file"),
+             file.errorString());
+             return;
+         }
+        }
+    //ecrireDot(fileName,EspaceTravail::g);//recuperer le graphe courant ;
+    QMessageBox::information(NULL, "Information!", "Fichier exporter en DOT !");
+}
+void PanneauOutils::nouveauFichier()
+{
+    const QMessageBox::StandardButton ret
+        = QMessageBox::warning(this, tr("Application"),
+                               tr("voulez vous sauvegarder ce fichier courant ?"),
+                               QMessageBox::Save | QMessageBox::Discard );
+    switch (ret) {
+    case QMessageBox::Save:
+        sauvegarderFichier();
+  /* case QMessageBox::Discard
+        je dois recuperer ce qu'il y'a dans la scene et le clean */
+    default:
+        break;
+    }
+   // EspaceTravail::scene->clear();
+    curFile.clear();
+    QMessageBox::information(NULL, "Information!", "Nouveau Fichier créer !");
+}

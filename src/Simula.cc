2e69c1061f9db2357b1fa6fe87d87e71cdda8e57
Mahdi LARBI <mahdi.larbi@ens.uvsq.fr>
2020-05-12 20:08:49 +0200

    GFICHIER fix

--- src/Simula.cc
+++ src/Simula.cc
@@ -1,16 +1,34 @@
-#include <QApplication>
-#include <QtWidgets>
+#include <iostream>
+#include <vector>
+#include "GFichier.hh"
+#include "Graphe.hh"
+#include "DataOutils.hh"
 
-#include "simulaGui.hh"
-
+using std::vector;
 
 
 int main( int argc, char ** argv)	{
-    QApplication app(argc,argv);
-    simulaGui test;
-    test.show();
 
+    string message = argv[1];
+    int n1 = atoi(argv[2]);
+    int n2 = atoi(argv[3]);
 
-    return app.exec();
+    Graphe * graphe = Graphe::get();
+    lireXml("test.xml");
+
+    Data * data = new Data(message);
+
+    Station * st = dynamic_cast<Station*>(graphe->getSommets()[n1]);
+
+    envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, true, false, st->getNextNumSeq(), 0, 100,true, data);
+     //envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, true, false, st->getNextNumSeq(), 0, 101, data2);
+     //envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, true, false, st->getNextNumSeq(), 0, 102, data3);
+   // Graphe::genererChemin(0, n1 ,n2, path, false);
+
+     //std::cout << st->getControleur()->getMapFileEnvoyer().size()<<std::endl;
+     st->getControleur()->verifieNbrSegment(st);
+
+     delete graphe;
 
 }
+/////

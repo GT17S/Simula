11b3ddb8a00a01e891ef03b2b9fe859735767193
LARBI Mahdi <larbi.mahdi@outlook.fr>
2020-05-12 20:12:12 +0200

    Update Simula.cc

--- src/Simula.cc
+++ src/Simula.cc
@@ -1,34 +1,16 @@
-#include <iostream>
-#include <vector>
-#include "GFichier.hh"
-#include "Graphe.hh"
-#include "DataOutils.hh"
+#include <QApplication>
+#include <QtWidgets>
 
-using std::vector;
+#include "simulaGui.hh"
+
 
 
 int main( int argc, char ** argv)	{
+    QApplication app(argc,argv);
+    simulaGui test;
+    test.show();
 
-    string message = argv[1];
-    int n1 = atoi(argv[2]);
-    int n2 = atoi(argv[3]);
 
-    Graphe * graphe = Graphe::get();
-    lireXml("test.xml");
-
-    Data * data = new Data(message);
-
-    Station * st = dynamic_cast<Station*>(graphe->getSommets()[n1]);
-
-    envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, true, false, st->getNextNumSeq(), 0, 100,true, data);
-     //envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, true, false, st->getNextNumSeq(), 0, 101, data2);
-     //envoyer(graphe->getSommets()[n1], graphe->getSommets()[n2], 1025, 80, true, false, st->getNextNumSeq(), 0, 102, data3);
-   // Graphe::genererChemin(0, n1 ,n2, path, false);
-
-     //std::cout << st->getControleur()->getMapFileEnvoyer().size()<<std::endl;
-     st->getControleur()->verifieNbrSegment(st);
-
-     delete graphe;
+    return app.exec();
 
 }
-/////

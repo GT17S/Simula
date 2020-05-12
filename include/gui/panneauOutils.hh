5325ccf03198e3a22541ea06fb7772755d6df3ea
GhilesBM <rilesbabou1@gmail.com>
2020-05-13 01:30:53 +0200

    ajout des slots faisant la gestion des timer

--- include/gui/panneauOutils.hh
+++ include/gui/panneauOutils.hh
@@ -5,7 +5,11 @@
 #include <QWidget>
 #include <QAction>
 #include <iostream>
+#include <QTimer>
+#include <QDebug>
+#include <QLabel>
 #include "EspaceTravail.hh"
+#include "gSimulation.hh"
 class PanneauOutils : public QToolBar{
 	Q_OBJECT
 public:
@@ -14,21 +18,22 @@ public:
 	virtual ~PanneauOutils(){};
 
 public slots:
+    void timer();
     void nouveauFichier();
     void ouvrirFichier();
     void sauvegarderFichier();
     void exporterFichier();
     void exporterImage(){std::cout<< "Exporter"<<std::endl;};
-    void demarrerSimulation(){std::cout<< "demarrer"<<std::endl;};
-    void arreterSimulation(){std::cout<< "arreter"<<std::endl;};
-    void resetSimulation(){std::cout<< "reset"<<std::endl;};
+    void demarrerSimulation();
+    void pauseSimulation();
+    void arreterSimulation();
+    void resetSimulation();
     void changeMode(){std::cout<< "changeMode"<<std::endl;};
     void envoieD(){std::cout<< "envoieD"<<std::endl;};
 
 private :
     QString curFile;
-
-    bool maybeSave();
+    gSimulation gestSimulation;
 };
 
 #endif

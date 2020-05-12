bf46cdf16e3513a89c7a7fa881066034f4ea76b4
Akli Hamitouche <akli.m.hamitouche@gmail.com>
2020-05-13 00:52:52 +0200

    Ajout du Panneau de Données et des onglets

---
+++ src/gui/PanneauData.cc
@@ -0,0 +1,20 @@
+#include "PanneauData.hh"
+
+PanneauData::PanneauData( QWidget *parent) : QTabWidget(parent) {
+	this->setMinimumHeight(80);
+	this->setMaximumHeight(100);
+	this->setTabsClosable(true);
+//	this->setStyleSheet("background-color: blue");
+//	this->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Expanding);
+}
+
+
+PanneauData::~PanneauData(){}
+
+void PanneauData::addData ( Data * d)	{
+
+	DataG * od = new DataG( d, this);
+//	od->setTabsClosable (true);
+	this->addTab( od,QString("Envoie n°%1").arg( onglets.size()+1));
+	onglets.insert ( d,od);
+}

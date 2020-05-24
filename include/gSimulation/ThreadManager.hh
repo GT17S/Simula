07b80c082bbff8055430e0b6ff0d692907893aa4
Akli Hamitouche <akli.m.hamitouche@gmail.com>
2020-05-24 06:30:55 +0200

    Ajout des fonctions de création de Thread quand une Station est créée (Ajout d'un vector de Station * pour identifier les différentes stations tournant sur un thread

--- include/gSimulation/ThreadManager.hh
+++ include/gSimulation/ThreadManager.hh
@@ -1,6 +1,7 @@
 #ifndef __HH_MANAGER__
 #define __HH_MANAGER__
 
+#include <QObject>
 
 #include <thread>
 #include <vector>
@@ -10,18 +11,26 @@
 
 #include "Data.hh"
 #include "Graphe.hh"
+#include "NoeudG.hh"
+#include "Station.hh"
 #include "gSimulation.hh"
 
 class gSimulation;
+class Station;
 
-class ThreadManager
-{
+class ThreadManager : public QObject {
+Q_OBJECT
 
 private:
 	std::vector<Data*> MovingData;
 	std::vector<std::thread> WorkingThreads;
 	std::map<std::string, std::mutex*> mutexmap;	
 	gSimulation* gestionnaire;
+	// Ajouté par Massi
+	std::vector<Station *> workingStations;
+	
+	
+	bool findWorker ( Station * s);
 public:
 	ThreadManager(gSimulation* gestionnaire);
 	~ThreadManager();
@@ -30,6 +39,9 @@ public:
 
 	
 	void joinall();
+public slots:
+	void createWorker ( NoeudG * n);
+
 };
 
 #endif

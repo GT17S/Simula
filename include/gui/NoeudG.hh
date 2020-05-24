39385fc4f1e4a7ec736e0b2d10bc7539af6f3c42
GT17S <guenanetoufik0615@gmail.com>
2020-05-24 23:47:54 +0200

    Documentation Gui

--- include/gui/NoeudG.hh
+++ include/gui/NoeudG.hh
@@ -64,28 +64,69 @@ public:
      */
     ~NoeudG();
 
+    /*!
+        * \brief getChild
+        * recuperer l'enfant de NoeudG qui est Un Noeud
+   */
     Noeud * getChild(){return child;}
+
+
+    /*!
+        * \brief setChild
+        * affecter un noeud fils à NoeudG.
+        * \param _child: voir #Noeud
+   */
     void setChild(Noeud * _child);
 
+
+    /*!
+        * \brief getCablesG
+        * recuperer les CableG de NoeudG.
+   */
     vector<cableG_extremite> getCablesG(){return extremiteG;}
+
+
+    /*!
+        * \brief setCableG
+        * Affecte un vecteur de cableG.
+        * \param _extrimiteG: voir #cableG_extremite
+   */
     void setCableG(vector<cableG_extremite> _extremiteG){ extremiteG = _extremiteG;}
 
+
+    /*!
+        * \brief getTreeItem
+        * recuperer le parent de NoeudG .
+   */
     QTreeWidgetItem * getTreeItem(){return parent;}
+
+
+    /*!
+        * \brief setTreeItem
+        * Initialise le parent de NoeudG.
+        * \param *_parent: voir #QTreeWidgetItem
+   */
     void setTreeItem(QTreeWidgetItem *_parent){ parent = _parent;}
 
     void addLine(CableG * cable, bool isPoint1);
     void moveCable(QPointF newPos);
+
+
+    /*!
+        * \brief ToolTipShow
+        * Affichage en hover souris des donnees d'un equipement.
+   */
     void toolTipShow();
 	//Ajouté par Massi
 	EspaceTravail * getEspaceTravail ()	{ return espaceTravail;};
 
 protected:
-    vector<cableG_extremite> extremiteG;
+    vector<cableG_extremite> extremiteG;/*!< Vecteur stockant les cablesG d'un NoeudG*/
     //fenetre en clickant sur l'un des noeuds
-    EspaceTravail *espaceTravail;
-    Noeud * child;
+    EspaceTravail *espaceTravail;/*!< Objet permettant de contenir les equipements et de realiser leurs differrentes interactions sur l'interface graphique*/
+    Noeud * child;/*!< Fils de NoeudG permettant d'affichage et la gestion des données sur l'interface graphique*/
     QTreeWidgetItem *parent;/*!< la section ou tous les traitement d'une station seront affiche dans PanneauEvent */
-    Dialog * configuration;
+    Dialog * configuration;/*!< fenetre de configuration du NoeudG sur l'interface graphique*/
 
     //void mouseDoubleClickEvent( QMouseEvent * e );
     void mousePressEvent(QGraphicsSceneMouseEvent *event);
--- include/gui/PanneauData.hh
+++ include/gui/PanneauData.hh
@@ -11,17 +11,39 @@
 #include "Graphe.hh"
 #include "QTreeWidgetItem"
 
+
+/*!
+ * \class PanneauData
+ * \brief La classe PanneauData représentant le panneau de la Donne traité sur l'interface graphique lors de la simulation du protocole TCP/IP.
+ */
 class PanneauData : public QTabWidget {
 Q_OBJECT
 public:
+
+    /*!
+        * \brief Constructeur paramétré
+        * Constructeur qui prend un parent QWidget.
+        * \param *parent : voir #QWidget
+   */
 	PanneauData( QWidget *parent = nullptr);
+
+    /*!
+        * \brief Destructeur
+        * Destructeur de la classe PanneauData
+    */
 	~PanneauData();
 public slots:
+    /*!
+        * \brief addData
+        * Ajout d'une donne sur le PanneauData
+         * \param *d: voir #Data
+
+*/
 	void addData ( Data * d);
 
 
 private:
-    QMap <Data *, DataG *> onglets;
+    QMap <Data *, DataG *> onglets;/*!< Onglets d'affichage de la donne des sa construction a sa destruction*/
 
 };
 

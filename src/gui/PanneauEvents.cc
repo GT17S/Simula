af65439f1e8d994ccd174615695ed5c9556dcb90
Nadjibian <nadj27@hotmail.fr>
2020-05-26 01:16:55 +0200

    effacer events

--- src/gui/PanneauEvents.cc
+++ src/gui/PanneauEvents.cc
@@ -26,22 +26,27 @@ PanneauEvents::PanneauEvents(){
     timerSimulation->setSegmentStyle(QLCDNumber::Flat);
     layout.addWidget(timerSimulation);
     timerSimulation->setMinimumHeight(30);
-    /*-
-    txt=new QTextEdit(this);
-    txt->setReadOnly(true);
-    //txt->setEnabled(false);
-    layout.addWidget(txt);
-    txt->setStyleSheet(QString::fromUtf8("background-color: rgb(122, 122, 122);"));
-    txt->setText(gSimulation::getTime()->toString("hh:mm:ss")+"  Debut de sumulation");
-*/
+
     layout.addWidget(treeview);
     treeview->setColumnCount(1);
     treeview->setColumnWidth(0, 500);
     QStringList ColumnNames;
     ColumnNames << "Équipements";
-
     treeview->setHeaderLabels(ColumnNames);
-    //treeview->setStyleSheet("background-color: red");//background
+
+    clear=new QPushButton("Effacer événements ");
+    layout.addWidget(clear);
+
+    connect(clear,SIGNAL(clicked()),this,SLOT(supItems()));
+}
+
+void PanneauEvents::supItems(){
+    for(Noeud *n: Graphe::getSommets()){
+       QTreeWidgetItem * tree=  n->getParent()->getTreeItem();
+
+               foreach(auto i, tree->takeChildren()) delete i;
+    }
+
 }
 void PanneauEvents::afftime(){
 
@@ -67,7 +72,6 @@ void PanneauEvents::addRoot(QTreeWidgetItem *pf ,QString nom){
 
 void PanneauEvents::addCh(QTreeWidgetItem *parent, QString nom){
     parent->setExpanded( true );
-    qDebug()<<"le nom est "+nom;
     QTreeWidgetItem *item1=new QTreeWidgetItem();
     parent->setChildIndicatorPolicy(QTreeWidgetItem::ShowIndicator);
     item1->setText(0,gSimulation::getTime()->toString()+" "+nom);
